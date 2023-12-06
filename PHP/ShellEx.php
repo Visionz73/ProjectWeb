f8c15654b990cfc2ea29880a3a809f03f6e9c6f7
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2023-12-06 16:40:18 +0100

    shell

--- PHP/ShellEx.php
+++ PHP/ShellEx.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-<form action="register.php" method="post">
+<form action="ShellEx.php" method="post">
     Username: <input type="text" name="username" required>
     Password: <input type="password" name="password" required>
     <input type="submit" value="Register">
