518ab4164a3181a6c0dac5783b92e6ea751c3c8b
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2023-12-06 14:52:50 +0100

    php

--- PHP/regon.php
+++ PHP/regon.php
@@ -12,5 +12,22 @@ if ($conn->connect_error) {
     die("Verbindung fehlgeschlagen: " . $conn->connect_error);
 }
 
+// SQL-Befehl zum Abrufen der Daten
+$sql = "SELECT username, passwort, email FROM Benutzer";
+$result = $conn->query($sql);
+
+// Überprüfen, ob Ergebnisse vorhanden sind
+if ($result->num_rows > 0) {
+    // Ergebnisse in einer Schleife ausgeben
+    while ($row = $result->fetch_assoc()) {
+        echo "Username: " . $row["username"] . "<br>";
+        echo "Passwort: " . $row["passwort"] . "<br>";
+        echo "Email: " . $row["email"] . "<br>";
+        echo "<hr>"; // Trennlinie zwischen den Datensätzen
+    }
+} else {
+    echo "Keine Ergebnisse gefunden";
+}
+
 $conn->close();
 ?>
