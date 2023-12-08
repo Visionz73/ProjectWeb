2465640f7f156c6e2a7acaab97f612e8da96b473
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2023-12-08 14:51:35 +0100

    cc

--- PHP/download.php
+++ PHP/download.php
@@ -10,7 +10,7 @@ if (file_exists($file)) {
     header('Content-Disposition: attachment; filename=' . basename($file));
     header('Content-Length: ' . filesize($file));
 
-    // Read the file and output it to the browser
+    // Read the file and output it to the browser files
     readfile($file);
     exit;
 } else {
