510ee399e949ad92b6bf5b8fe15ad663f78d3a86
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2023-12-18 12:26:31 +0100

    cc

--- PHP/logon1.php
+++ PHP/logon1.php
@@ -1,5 +1,27 @@
-<?
+<?php
 
-print_r($_POST);
+// Verbindung zur Datenbank herstellen
+$servername = "localhost";
+$username = "root";
+$password = "neues-passwort";
+$dbname = "BenutzerDatenbank";
 
+
+$benutzername = $_POST["username"];
+$passwort = $_POST["passwort"];
+
+
+if($coonector->login($benutzername, $passwort) == true){
+    session_start();
+    $_SESSION['user'] =  $benutzername;
+    header("Location: ../html/home_login.html");   
+}
+
+
+ else {
+    echo "Login fehlgeschlagen. Benutzerdaten nicht gefunden.";
+}
+
+// // Verbindung schließen
+// $conn->close();
 ?>
