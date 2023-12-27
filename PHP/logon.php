69fcc23af6e81faefd1d183c3284a0c4da7df9cf
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2023-12-27 23:56:32 +0100

    cc

--- PHP/logon.php
+++ PHP/logon.php
@@ -1,3 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/logon.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+
+
+
+<body>
+
+
+    <header class="header">
+        <a href="#" class="logo">OwnCloud<i class='bx bx-cloud' ></i> </a>
+
+        <nav class="navbar">
+            <a href="../PHP/home.php" class="active">Home</a>
+            <a href="#">About</a>
+            <a href="#">Review</a>
+            <a href="../html/regon.html">Sign Up</a>
+            <a href="../html/logon.html">Login</a>
+        </nav>
+
+        <div class="social-media">
+            <a href="#"><i class='bx bxl-instagram-alt' ></i></a>
+            <a href="#"><i class='bx bxl-meta' ></i></a>
+            <a href="#"><i class='bx bxl-reddit' ></i></a>
+        </div>
+    </header>
+
+    <div class="wrapper">
+        <form action="../PHP/logon.php" method="post">
+            <h1>Login</h1>
+            <div class= "input-box">
+                <input type="text" name="username" placeholder="Username" required>
+                <i class='bx bxs-user'></i> 
+            </div>
+            <div class="input-box">
+                <input type="password" name="passwort" placeholder="password" required>
+                <i class='bx bxs-lock-alt' ></i>
+            </div>
+
+            <div class="remember-forgot">
+                <label><input type="checkbox"> Remember me</label>
+                <a href="#">Forgot Password?</a>
+            </div>
+
+            <button type="submit" class="btn">Login</button>
+
+            <div class="register-link">
+                <p>Dont have an account? <a href="../html/regon.html">Register</a></p>
+            </div>
+
+        </form>
+    </div>
+
+
+
+        
+    
+</body>
+
+</html>
+
 <?php
 require_once('Authentication.php');
 // Verbindung zur Datenbank herstellen
