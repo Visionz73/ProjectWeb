237aaa5aa7eb6d6afa681bea557e3ad44d4d05bb
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2023-12-27 23:51:44 +0100

    cc

--- PHP/fileshare.php
+++ PHP/fileshare.php
@@ -38,7 +38,7 @@
                     session_start();
                     if (isset($_SESSION["user"])) {
                 ?>
-                        <a href="../html/home.html" class="active">Home</a>
+                        <a href="../PHP/home.php" class="active">Home</a>
                         <a href="../PHP/fileshare.php">FileShare</a>
                         <a href="../PHP/logout.php">Logout</a>
                 <?php
