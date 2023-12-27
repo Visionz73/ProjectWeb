8a852e25181792be02fc362dc27dff700fd677a6
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2023-12-27 23:27:24 +0100

    navbar fix

--- PHP/home.php
+++ PHP/home.php
@@ -14,32 +14,25 @@
     <header class="header">
         <a href="#" class="logo">OwnCloud<i class='bx bx-cloud' ></i> </a>
 
-        <nav class="navbar">
-            <a href="../html/home.html" class="active">Home</a>
-            <a href="#">About</a>
-            <a href="#">Review</a>
-            <a href="../html/regon.html">Sign Up</a>
-            <a href="../html/logon.html">Login</a>
-
-            
+                <nav class="navbar">
                 <?php
                     session_start();
-                    if(isset($_SESSION["user"])) {
-                    ?>
+                    if (isset($_SESSION["user"])) {
+                ?>
+                        <a href="../html/home.html" class="active">Home</a>
                         <a href="../PHP/fileshare.php">FileShare</a>
                         <a href="../PHP/logout.php">Logout</a>
-                    <?php
-                    } 
-
-                    else {
-                        echo("no user in SESSION"); //TEST PHASE
+                <?php
+                    } else {
+                ?>
+                        <a href="../html/home.html" class="active">Home</a>
+                        <a href="../html/regon.html">Sign Up</a>
+                        <a href="../html/logon.html">Login</a>
+                <?php
                     }
                 ?>
-                
-                
-        
+</nav>
 
-        </nav>
 
         <div class="social-media">
 
