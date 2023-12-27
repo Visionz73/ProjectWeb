985ce2d792cf57904b00e3a70707b3466860a7d1
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2023-12-27 16:10:08 +0100

    utf8

--- SQL_DUMP/BenutzerDatenbank_backup.sql
+++ SQL_DUMP/BenutzerDatenbank_backup.sql
@@ -25,10 +25,10 @@ DROP TABLE IF EXISTS `Benutzer`;
 CREATE TABLE `Benutzer` (
   `UserID` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
-  `passwort` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `passwort` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
