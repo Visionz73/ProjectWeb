06fc54844a098650a2084d44d19d24945b3b604a
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2023-12-28 18:46:09 +0100

    cc

---
+++ Install_Script/gesamt_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Starte Skriptprozess..."
+chmod +x Backup/benutzer_importieren.sh
+chmod +x ./tool_config.sh
+
+
+# Ausführen von Skript 1
+if ./tool_config.sh; then
+    echo "Die Basis Programme wurden installiert und erfolgreich abgeschlossen."
+else
+    echo "Fehler bei der Ausführung von Tools und Configs."
+    exit 1
+fi
+
+# Ausführen von Skript 2
+if Backup/./benutzer_importieren.sh; then
+    echo "Die Basis user wurden eingerichtet und erfolgreich abgeschlossen."
+else
+    echo "Fehler bei der Ausführung von user and priv."
+    exit 1
+fi
+
+# Weitere Skripte können hier in ähnlicher Weise hinzugefügt werden...
+
+echo "Alle Skripte wurden erfolgreich ausgeführt."
