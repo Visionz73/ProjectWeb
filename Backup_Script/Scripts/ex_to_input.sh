b5e378efa046afd4b568a38a4c50fd0fd3249720
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2024-01-05 00:54:17 +0100

    backup Script

--- Backup_Script/Scripts/ex_to_input.sh
+++ Backup_Script/Scripts/ex_to_input.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp -R /home/adminrv/ProjectWeb/Backup_Script/Backup/Export_user/* /home/adminrv/ProjectWeb/Install_Script/Backup/Import_user/
+cp -R ../Backup/Export_user/ /home/adminrv/ProjectWeb/Install_Script/Backup/Import_user
 
-cp -R /home/adminrv/ProjectWeb/Backup_Script/Databases/BenutzerDatenbank.sql* /home/adminrv/ProjectWeb/Install_Script/Databases/
+cp -R ../Databases/BenutzerDatenbank.sql* /home/adminrv/ProjectWeb/Install_Script/Database/
