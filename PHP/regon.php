8a285abaf8aad185612ff7f60e3df361d3451190
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2024-01-10 11:42:40 +0100

    fix regon

--- PHP/regon.php
+++ PHP/regon.php
@@ -1,75 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="../CSS/logon.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
-    
-
-
 </head>
-
-
-
 <body>
 
 <header class="header">
-        <a href="#" class="logo">OwnCloud<i class='bx bx-cloud' ></i> </a>
-
-        
-
-                <nav class="navbar">
-                <?php
-                     
-                    session_start();
-                    if (isset($_SESSION["user"])) {
-                ?>
-                        <a href="../PHP/home.php" class="active">Home</a>
-                        <a href="../PHP/fileshare.php">FileShare</a>
-                        <a href="../PHP/logout.php">Logout</a>
-                <?php
-                    } else {
-                ?>
-                        <a href="../PHP/home.php" class="active">Home</a>
-                        <a href="../PHP/logon.php">Login</a>
-                <?php
-                    }
-                ?>
-</nav>
-
-
-<div class="social-media">
-
+    <a href="#" class="logo">OwnCloud<i class='bx bx-cloud' ></i></a>
+    <nav class="navbar">
         <?php
-            session_start();
-
-            if (isset($_SESSION["user"])) {
-                $user = htmlspecialchars($_SESSION["user"]); // Sicherstellen, vor cross side scripting (htmlspecialchars)
-                
+        session_start();
+        if (isset($_SESSION["user"])) {
+            ?>
+            <a href="../PHP/home.php" class="active">Home</a>
+            <a href="../PHP/fileshare.php">FileShare</a>
+            <a href="../PHP/logout.php">Logout</a>
+            <?php
+        } else {
+            ?>
+            <a href="../PHP/home.php" class="active">Home</a>
+            <a href="../PHP/logon.php">Login</a>
+            <?php
+        }
         ?>
-                <a href=""><i class='bx bxs-invader'></i><?php echo $user; ?></a>
+    </nav>
+    <div class="social-media">
         <?php
-            } else {
+        if (isset($_SESSION["user"])) {
+            $user = htmlspecialchars($_SESSION["user"]);
+            ?>
+            <a href=""><i class='bx bxs-invader'></i><?php echo $user; ?></a>
+            <?php
+        } else {
+            ?>
+            <a href="../PHP/regon.php">Sign Up</a>
+            <?php
+        }
         ?>
-                <a href="../PHP/regon.php">Sign Up</a>
-        <?php
-            }
-        ?>
+    </div>
+</header>
 
-</div>
-
-    </header>
-
-    <?php
-// Verbindung zur Datenbank herstellen
+<?php
 $registrationMessage = "";
-
-
-    
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require_once('Authentication.php');
 
@@ -79,71 +55,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $dbname = "BenutzerDatenbank";
 
     $username_Web = $_POST['username'];
-    $passwort_Web = $_POST['passwort']; 
+    $passwort_Web = $_POST['passwort'];
     $email_Web = $_POST['email'];
 
     $connector = new Authentication($servername, $dbUsername, $dbPassword, $dbname);
     if ($connector->register($username_Web, $passwort_Web, $email_Web)) {
-        // Benutzer wurde erfolgreich in der Datenbank registriert
-        $username = escapeshellarg($username_Web); // Verwenden Sie escapeshellarg für Sicherheit
+        $username = escapeshellarg($username_Web);
 
         $createUserCommand = "sudo useradd -m $username";
         $setPrivCommand = "sudo chown -R www-data:www-data /home/$username";
-        
-        echo "<pre>";
-        echo shell_exec($createUserCommand);
-        echo shell_exec($setPrivCommand);
-        echo "</pre>";
 
+        shell_exec($createUserCommand);
+        shell_exec($setPrivCommand);
 
         $registrationMessage = "<div class='success-message'>Benutzer erfolgreich registriert.</div>";
-        echo "$registrationMessage";
-
-        
     } else {
-        echo "Fehler bei der Registrierung.";
+        $registrationMessage = "<div class='error-message'>Fehler bei der Registrierung.</div>";
     }
 
     $connector->close();
 }
+?>
 
-
-    <div class="wrapper">
-        <form action="../PHP/regon.php" method="post">
-            <h1>Sign Up</h1>
-            <div class= "input-box">
-                <input type="text" name="username" placeholder="Username" required>
-                <i class='bx bxs-user'></i> 
-            </div>
-            <div class="input-box">
-                <input type="password" name="passwort" placeholder="password" required>
-                <i class='bx bxs-lock-alt' ></i>
-            </div>
-            <div class="input-box">
-                <input type="text" name="email" placeholder="Mail" required>
-                <i class='bx bxs-envelope' ></i>
-            </div>
-
-            
-
-            <button type="submit" class="btn">Sign Up</button>
-
-            <div class="register-link">
-                <p>Already have an account? <a href="../PHP/regon.php">Login</a></p>
-                if {
-                    
-                }
-            </div>
-
-        </form>
-    </div>
-
-
-
-        
-    
-</body>
-
-</html>
-
-
+<div class="wrapper">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <h1>Sign Up</h1>
+        <div class="input-box">
+            <input type="text" name="username" placeholder="Username" required>
+            <i class='bx bxs-user'></i>
+        </div>
+        <div
