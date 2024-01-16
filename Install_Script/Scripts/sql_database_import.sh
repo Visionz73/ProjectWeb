cd15b0853f18c763854a6621a5e40e2fc2657eb7
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2024-01-16 22:17:54 +0100

    skript

--- Install_Script/Scripts/sql_database_import.sh
+++ Install_Script/Scripts/sql_database_import.sh
@@ -2,7 +2,7 @@
 sed -i 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g' ../Database/BenutzerDatenbank.sql
 
 # Erstellen der Datenbank, falls sie nicht existiert
-sudo mariadb -u root -p -e "CREATE DATABASE IF NOT EXISTS BenutzerDatenbank;"
+sudo mariadb -u root -p'12345' -e "CREATE DATABASE IF NOT EXISTS BenutzerDatenbank;"
 
 # Importieren der Datenbank
-sudo mariadb -u root -p BenutzerDatenbank < ../Database/BenutzerDatenbank.sql
+sudo mariadb -u root -p'12345' BenutzerDatenbank < ../Database/BenutzerDatenbank.sql
